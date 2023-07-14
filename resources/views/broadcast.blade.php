34134cfcf77506129ff7821f180a83801f7b0a90
Elvis Poenaru <elvis.poe96@yahoo.com>
2023-07-14 11:27:00 +0300

    App Install and Chat dev

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,11 @@
+<div class="flex items-center message justify-end">
+
+    <p class="text-gray-500 px-6 dark:text-gray-400 text-sm leading-relaxed">
+        {{ $message }}
+    </p>
+
+    <div class="h-16 w-16 flex items-center justify-center rounded-full">
+        <img src="https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133351928-stock-illustration-default-placeholder-man-and-woman.jpg" alt="You">
+    </div>
+</div>
+                        
